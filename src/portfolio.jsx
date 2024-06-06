66e81ef9414e4859d3bc5e86ca9e2fccb52f9dbc
Imam ariadi <gamerapin139@gmail.com>
2024-06-06 11:05:58 +0700

    modif dikit

--- src/portfolio.jsx
+++ src/portfolio.jsx
@@ -4,17 +4,17 @@ function PortfolioControls({ setFilter }) {
   const handleClick = (category) => () => setFilter(category);
 
   return (
-    <div className="controls text-center">
-      <button className="filter btn btn-common" onClick={handleClick('all')}>
+    <div class="controls text-center" className="controls text-center">
+      <button class="filter active btn btn-common" className="filter btn btn-common" onClick={handleClick('all')}>
         All
       </button>
-      <button className="filter btn btn-common" onClick={handleClick('design')}>
+      <button class="filter btn btn-common" className="filter btn btn-common" onClick={handleClick('design')}>
         UI/UX Designers
       </button>
-      <button className="filter btn btn-common" onClick={handleClick('development')}>
+      <button class="filter btn btn-common" className="filter btn btn-common" onClick={handleClick('development')}>
         Fullstack Development
       </button>
-      <button className="filter btn btn-common" onClick={handleClick('print')}>
+      <button class="filter btn btn-common" className="filter btn btn-common" onClick={handleClick('print')}>
         Graphic Design
       </button>
     </div>
